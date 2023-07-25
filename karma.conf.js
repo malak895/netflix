0dcd7905c3fe162f2492b926e99686ec7b4a3eea
malak895 <malak.hammami263@gmail.com>
2023-07-25 16:09:01 +0100

    first commit

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/showtime'),
+      dir: require('path').join(__dirname, './coverage/netflix'),
       subdir: '.',
       reporters: [
         { type: 'html' },
